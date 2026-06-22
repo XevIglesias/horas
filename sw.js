ae9ef40c1c95de00c6b135c789473a9f745c8936
Xavier Iglesias <xevi2iglesias@gmail.com>
2026-06-22 09:22:05 +0200

    Cuadrante de horas y extras (PWA, plantilla en blanco)

---
+++ sw.js
@@ -0,0 +1,29 @@
+/* Service worker: guarda la app para que funcione sin internet una vez abierta. */
+const CACHE = 'cuadrante-v2';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(resp => {
+      if (resp && resp.status === 200 && new URL(req.url).origin === location.origin) {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+      }
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
