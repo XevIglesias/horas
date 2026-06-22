2a5e65c557aaaba8a7ecb13d50fa797dcf98b530
Xavier Iglesias <xevi2iglesias@gmail.com>
2026-06-22 09:40:23 +0200

    Quitar copia/restaurar: la app guarda sola en el dispositivo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker: guarda la app para que funcione sin internet una vez abierta. */
-const CACHE = 'cuadrante-v2';
+const CACHE = 'cuadrante-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
