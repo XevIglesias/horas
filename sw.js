4c6713f354667cbc71b40db7912a31f79305bb8a
Xavier Iglesias <xevi2iglesias@gmail.com>
2026-06-25 08:54:36 +0200

    Festivos: marcar dias festivos y aplicar plus festivo (11,27 EUR/h)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker: guarda la app para que funcione sin internet una vez abierta. */
-const CACHE = 'cuadrante-v3';
+const CACHE = 'cuadrante-v4';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
